b2d5660c4e31ce2f9edb5959daf34c561f165693
SSharWorks <SharCoding@gmail.com>
2020-08-23 03:54:52 +0400

    Updating Assets folder

---
+++ Assets/Scripts/GameManager/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartButton()
+    {
+        SceneManager.LoadScene("Room001");
+    }
+
+    public void LevelSelector()
+    {
+        return;
+    }
+
+    public void Shop()
+    {
+        return;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/GameManager/test.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+
+    private Transform cameraTransform;
+    [SerializeField] private Transform Canvas01;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraTransform = FindObjectOfType<Camera>().transform;
+    }
+
+    public void moveToNextScene()
+    {
+        StartCoroutine(delayCameraMovement());
+    }
+
+    IEnumerator delayCameraMovement()
+    {
+        while (cameraTransform.position.x < 22f)
+        {
+            float xPos = cameraTransform.position.x + 1f;
+            cameraTransform.position = new Vector3(xPos, 0, 0);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+}
