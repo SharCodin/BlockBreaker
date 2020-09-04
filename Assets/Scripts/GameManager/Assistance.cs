788b3c5a7d08fc20e66863dfe8566716b6505852
SSharWorks <SharCoding@gmail.com>
2020-09-04 19:11:09 +0400

    Prototype to assist mode. Change titlescreen

---
+++ Assets/Scripts/GameManager/Assistance.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Assistance : MonoBehaviour
+{
+
+    public static bool isAssisted = false;
+
+    public void AssistedMode()
+    {
+        if (isAssisted) 
+            isAssisted = false;
+        else 
+            isAssisted = true;
+    }
+
+}
